613b0096bd9597b4d21f6a887f18a4ffe8a89f15
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-05-21 02:07:05 +0200

    Fix: navbar fixed only for larg sceens

--- src/js/customNavbar.js
+++ src/js/customNavbar.js
@@ -1,12 +1,18 @@
 const headerNavbar = document.querySelector('.header');
+const logoNavbar = document.querySelector('.header__logo');
+const contactNavbar = document.querySelector('.header__contact');
 
 window.onscroll = function () {
   // document.body.scrollTop for firefox, IE
   // document.documentElement.scrollTop for Chrome
 
   if (document.documentElement.scrollTop || document.body.scrollTop > 10) {
-    headerNavbar.classList.add('scroll-js');
+    headerNavbar.classList.add('header-scroll-js');
+    logoNavbar.classList.add('logo-scroll-js');
+    contactNavbar.classList.add('contact-scroll-js');
   } else {
-    headerNavbar.classList.remove('scroll-js');
+    headerNavbar.classList.remove('header-scroll-js');
+    logoNavbar.classList.remove('logo-scroll-js');
+    contactNavbar.classList.remove('contact-scroll-js');
   }
 };
